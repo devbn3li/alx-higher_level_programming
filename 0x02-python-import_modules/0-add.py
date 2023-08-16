75cccdf094a782e680c198ae60af9eb267196fa3
devbn3li <fudwhc@gmail.com>
2023-08-16 03:13:39 +0300

    g

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,4 @@ a = 1
 b = 2
 
 if __name__ == "__main__":
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/magic_calculation_102.py
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add(a, b):
-    return a + b
+	return a + b
 
 def sub(a, b):
-    return a - b
+	return a - b
