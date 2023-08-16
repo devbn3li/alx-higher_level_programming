229fb93693d737cef6f34da5a1ecfb802af5a3a0
devbn3li <fudwhc@gmail.com>
2023-08-16 04:38:44 +0300

    e

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -23,6 +23,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif
