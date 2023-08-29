ed2f2ef0c522938ac4609eeb61cb1066f8963d95
devbn3li <fudwhc@gmail.com>
2023-08-29 16:15:19 +0300

    l

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 from sys import stderr
+
 
 def safe_print_integer_err(value):
     try:
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def safe_function(fct, *args):
     try:
