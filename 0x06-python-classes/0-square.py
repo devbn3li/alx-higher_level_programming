3563a070d68f1a364ba0e38d4e4a2d1977e2098b
devbn3li <fudwhc@gmail.com>
2023-08-30 04:16:03 +0300

    python

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""class that represents a square"""
+
+
+class Square:
+    """class represents a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class represents a square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Init new Square
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
