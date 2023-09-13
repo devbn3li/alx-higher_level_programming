ed6ee8e5c8f00664cf875b3a79793ac8ba091b69
devbn3li <fudwhc@gmail.com>
2023-09-13 05:23:51 +0300

    jjii

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -22,14 +22,14 @@ if __name__ == "__main__":
     import sys
 
     size = 0
-    status_code = {}
+    status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
         for line in sys.stdin:
             if count == 10:
-                print_stats(size, status_code)
+                print_stats(size, status_codes)
                 count = 1
             else:
                 count += 1
@@ -44,14 +44,14 @@ if __name__ == "__main__":
             try:
                 if line[-2] in valid_codes:
                     if status_code.get(line[-2], -1) == -1:
-                        status_code[line[-2]] = 1
+                        status_codes[line[-2]] = 1
                     else:
-                        status_code[line[-2]] += 1
+                        status_codes[line[-2]] += 1
             except IndexError:
                 pass
 
-        print_stats(size, status_code)
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-        print_stats(size, status_code)
+        print_stats(size, status_codes)
         raise
