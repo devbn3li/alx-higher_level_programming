d307c3d9bea36ddb4e48777bfe552a3030e51f63
devbn3li <fudwhc@gmail.com>
2023-10-18 00:51:00 +0300

    pushd 0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- displays average temperature in Fahrenheit by city ordered by descending temperature
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
