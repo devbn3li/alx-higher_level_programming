af8ac2d8b54cff3088afef1679a80246a4b8f581
devbn3li <fudwhc@gmail.com>
2023-10-20 00:50:10 +0300

    fixing the commit

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,13 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
-SELECT DISTINCT gen.name
+SELECT DISTINCT g.name
 FROM tv_genres g
 INNER JOIN tv_show_genres s ON g.id = s.genre_id
 INNER JOIN tv_shows t ON s.show_id = t.id
-WHERE gen.name NOT IN (
-  SELECT DISTINCT gen2.name
-  FROM tv_genres gen2
-  INNER JOIN tv_show_genres s2 ON gen2.id = s2.genre_id
+WHERE g.name NOT IN (
+  SELECT DISTINCT g2.name
+  FROM tv_genres g2
+  INNER JOIN tv_show_genres s2 ON g2.id = s2.genre_id
   INNER JOIN tv_shows t2 ON s2.show_id = t2.id
   WHERE t2.title = 'Dexter'
 )
-ORDER BY gen.name ASC;
+ORDER BY g.name ASC;
