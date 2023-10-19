77591d3907d289f254ffa5789cdb48669f187fae
devbn3li <fudwhc@gmail.com>
2023-10-20 00:37:13 +0300

    push 0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server.s
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
