c2a85b91f1663590ac00d33a9b7e83995fb70c5b
devbn3li <fudwhc@gmail.com>
2023-10-20 00:03:13 +0300

    poopo

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- in the MySQL server
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost';
