9f5729a8a9755d5f0d3867236aeffc9704c9d7fd
devbn3li <fudwhc@gmail.com>
2023-11-14 00:22:54 +0200

    push 0x12-javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const x = process.argv[2];
-
-if (isNaN(x) || x === undefined) {
-    console.log("Missing number of occurrences");
-} else {
-    const num = parseInt(x, 10);
-    for (let i = 0; i < num; i++) {
-        console.log("C is fun");
-    }
+const count = parseInt(process.argv[2]);
+if (isNaN(count)) {
+    console.log('Missing number of occurrences');
+}
+for (let i = 0; i < count; i++) {
+    console.log('C is fun');
 }
