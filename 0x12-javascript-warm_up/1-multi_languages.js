bc4b649c64003079d3aa6d62956f7667edc64f6d
devbn3li <fudwhc@gmail.com>
2023-11-13 20:42:38 +0200

    push

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-let myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
