fcf84157ee376b65690d536ba12393f5e05dbb8d
devbn3li <fudwhc@gmail.com>
2023-11-15 03:54:18 +0200

    push 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const list = require('./100-data').list;
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+
+console.log(list);
+console.log(newList);
