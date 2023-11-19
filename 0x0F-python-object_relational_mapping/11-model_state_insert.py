120ff7f45533a09df6a8c1a1db9ffecbbf3de06f
devbn3li <fudwhc@gmail.com>
2023-11-20 01:10:08 +0200

    push 0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """script that adds the State object “Louisiana” 
 to the database hbtn_0e_6_usa"""
-
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="Louisiana")
-
-    session.add(new_state)
-
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
     session.commit()
-    print(new_state.id)
+    print(louisiana.id)
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -2,20 +2,18 @@
 """script that changes the name of a State object 
 from the database hbtn_0e_6_usa
 """
-
-
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     state = session.query(State).filter_by(id=2).first()
-    if state:
-        state.name = "New Mexico"
-        session.commit()
+    state.name = "New Mexico"
+    session.commit()
