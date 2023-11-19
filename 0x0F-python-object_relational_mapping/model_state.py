deab408eab4a76f2ead1dc1b9fb1592df5c8a569
devbn3li <fudwhc@gmail.com>
2023-11-20 00:40:43 +0200

    push 0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,12 +2,15 @@
 """Script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument"""
 
-import sys
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
+metadata = MetaData()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+class State (Base):
+    """class inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
