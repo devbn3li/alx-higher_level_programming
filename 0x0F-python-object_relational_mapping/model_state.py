ac6b77e35c3116110b6d2e7976b723078da79858
devbn3li <fudwhc@gmail.com>
2023-11-20 00:28:55 +0200

    push 0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,17 +2,18 @@
 """Script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument"""
 
-import MySQLdb
 import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY %s
-                ORDER BY id ASC""", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+
