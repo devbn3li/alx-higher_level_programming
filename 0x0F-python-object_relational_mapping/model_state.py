58111ce8975d8011b92714047bc02b28b0f550e8
devbn3li <fudwhc@gmail.com>
2023-11-20 00:32:24 +0200

    push 0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,26 +4,10 @@ table of hbtn_0e_0_usa where name matches the argument"""
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    """Engine creation"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-
-    """Session creation"""
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    """Query"""
-    query = session.query(State).filter(State.name == sys.argv[4]).first()
-    if query is None:
-        print("Not found")
-    else:
-        print(query.id)
-    session.close()
-
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
