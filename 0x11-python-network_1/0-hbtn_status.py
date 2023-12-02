c8e7d2e24f17a43108c927b83c30fea4787f3378
devbn3li <fudwhc@gmail.com>
2023-12-02 02:59:17 +0200

    push 0x11-python-network_1/0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
