db8f32e1356451a82a9c0b79f0714fe48f57cff2
devbn3li <fudwhc@gmail.com>
2024-01-03 19:17:49 +0200

    style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) { console.log(err); } else { console.log(data); }
-}
-);
+const fs = require("fs");
+fs.readFile(process.argv[2], "utf8", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
