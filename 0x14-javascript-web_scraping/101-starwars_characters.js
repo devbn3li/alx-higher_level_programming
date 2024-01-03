2ac2659db6b324b166e7b42405b377eec49d1086
devbn3li <fudwhc@gmail.com>
2024-01-03 21:27:57 +0200

    push

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const request = require("request");
-const url = "https://swapi.dev/api/films/" + process.argv[2];
+const request = require('request');
+const url = 'https://swapi.dev/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
-    let characters = JSON.parse(body).characters;
+    const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
 
-function printCharacters(characters, index) {
+function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
